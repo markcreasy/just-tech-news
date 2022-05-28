88f704090a70981c768f76cb5a598ba3f9bf108f
Mark Creasy <mcreasy50@gmail.com>
2022-05-28 10:55:41 -0400

    implement password hashing via bcrypt

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,7 @@ const { User } = require('../../models');
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
   User.findAll({
-    attributes: { exclude: ['password'] }
+    // attributes: { exclude: ['password'] }
   })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -56,6 +56,7 @@ router.put('/:id', (req, res) => {
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
