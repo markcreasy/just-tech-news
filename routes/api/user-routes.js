0fb08ee84cd450d86dcb4b4ca09462ac3b327282
Mark Creasy <mcreasy50@gmail.com>
2022-05-28 14:27:22 -0400

    password hashing and login functionality

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,32 @@ router.post('/', (req, res) => {
     });
 });
 
+// POST /api/login
+router.post('/login', (req, res) => {
+// expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+  });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
