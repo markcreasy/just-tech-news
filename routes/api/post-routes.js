fef1121f837bd95cf1c0dc396e02c8009d1d5135
Mark Creasy <mcreasy50@gmail.com>
2022-05-29 10:31:27 -0400

    voting functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -6,7 +6,13 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     order: [['created_at', 'DESC']],
     include: [
       {
@@ -28,7 +34,13 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: User,
@@ -65,34 +77,13 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-  // create the vote
-Vote.create({
-  user_id: req.body.user_id,
-  post_id: req.body.post_id
-}).then(() => {
-  // then find the post we just voted on
-  return Post.findOne({
-    where: {
-      id: req.body.post_id
-    },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-    .then(dbPostData => res.json(dbPostData))
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
-  });
 });
 
 router.put('/:id', (req, res) => {
