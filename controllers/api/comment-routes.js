fbb465ddf11bbf9d0751fd2ddcd1cf724e676696
Mark Creasy <mcreasy50@gmail.com>
2022-05-31 14:08:19 -0400

    conditional html - show/hide comment/upvote based on loggedin session

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,16 +13,20 @@ router.get('/', (req, res) => {
 
 // POST /api/comments
 router.post('/', (req, res) => {
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // check the session
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
 // DELETE /api/comments/1
