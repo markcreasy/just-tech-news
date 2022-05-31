c47a48f2ffa2b08e989a7fa4540d5c10a40ee911
Mark Creasy <mcreasy50@gmail.com>
2022-05-31 14:14:46 -0400

    show/hide login/logout based on session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       // pass a single post object into the homepage template
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
