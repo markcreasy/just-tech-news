5cfbd653d9b086200676b003ebfacb5cbe7691ab
Mark Creasy <mcreasy50@gmail.com>
2022-05-31 16:05:15 -0400

    setup dashboard views, and hook in new post form

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -82,7 +82,7 @@ router.post('/', (req, res) => {
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id
+    user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -43,7 +43,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
